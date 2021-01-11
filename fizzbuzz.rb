83f86ba9afe4307a3b8d4cdb6523a52cfeb2b07b
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-01-11 22:46:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,7 +3,12 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
-  end
+  elsif int % 5 == 0
+    "Buzz"
+  elsif ((int % 3 == 0) && (int % 5 == 0))
+    "FizzBuzz"
+  else
+    nil
 end
 
 #fizzbuzz(int)
